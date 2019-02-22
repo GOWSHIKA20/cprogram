e6a8e70b58ba6176410d077e25260512aa8f9fa1
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-02-22 16:30:27 +0530

    Create set_2_1.c

---
+++ set_2_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int n,k,p=0;
+clrscr();
+printf("enter the number and k value");
+scanf("%d%d",&n,&k);
+p=pow(n,k);
+printf("%d",p);
+return 0;
+}
