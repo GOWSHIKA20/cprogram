c80e62aaf06ef32420ff7db9d636b6e54f11f22b
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-02-27 16:32:34 +0530

    Create set1_6.c

---
+++ set1_6.c
@@ -0,0 +1,14 @@
+# cprog#include <stdio.h>
+
+int main()
+{
+    int yr;
+    printf("enter the yr");
+    scanf("%d",&yr);
+    if(yr%4==0)
+    printf("leap year");
+    else
+    printf("not a leap year");
+    return 0;
+}
+ram
