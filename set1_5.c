1e4471c474cefe65bf8d1c5fab0d3d8a6298a704
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-02-27 16:26:35 +0530

    Create set1_5.c

---
+++ set1_5.c
@@ -0,0 +1,22 @@
+# cpr#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+    {
+    printf("a is greater");
+    }
+    else if(b>a&&b>c)
+    {
+    printf("b is greater");
+    }
+    else
+    {
+    printf("c is greater");
+    }
+    return 0;
+}
+ogram
