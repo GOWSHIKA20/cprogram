7b1a48f3510461c4ebab6fffc17817dd3fbf3cd0
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-02-27 16:16:12 +0530

    Create set1_4.c

---
+++ set1_4.c
@@ -0,0 +1,14 @@
+# c#include <stdio.h>
+
+int main()
+{
+    char ch;
+    printf("enter the ch");
+    scanf("%c",&ch);
+    if(ch>='a'&&ch<='z')
+    printf("alphabet");
+    else
+    printf("number");
+     return 0;
+}
+program
