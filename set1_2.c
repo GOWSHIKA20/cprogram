01ee91945f3777c44345bb408bc0693ce7ce94d4
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-02-27 15:45:37 +0530

    Create set1_2.c

---
+++ set1_2.c
@@ -0,0 +1,14 @@
+# cprogra#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("even");
+    else 
+    printf("odd");
+    
+ return 0;
+}m
