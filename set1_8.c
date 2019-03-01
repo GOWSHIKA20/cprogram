908a5fb937a7a5335df40c0a3ed37517c5be1f31
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-03-01 15:31:05 +0530

    Create set1_8.c

---
+++ set1_8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int n,i,sum=0;
+printf("enter the n:");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+    return 0;
+}
