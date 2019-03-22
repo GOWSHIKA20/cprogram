ce434fa17283df954253979feb1ecd82b604c318
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-03-22 20:08:37 +0530

    Create set2_6.c

---
+++ set2_6.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int a,b,i,j;
+    printf("enter the a &b:");
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        for(j=2;j<=i;j++)
+        {
+            if(i%j==0)
+            break;
+        }
+            if(i==j)
+            printf("%d",j);
+        
+    }
