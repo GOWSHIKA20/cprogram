8a688ff149fce10bba5185f8973e0618f6a351f1
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-03-22 16:30:43 +0530

    Create set2_5.c

---
+++ set2_5.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int a,b,i,l=0;
+    printf("enter the a:");
+    scanf("%d",&a);
+    printf("enter the b:");
+    scanf("%d",&b);
+    for(i=a+1;i<b;i++)
+    {
+        if(i%2==0)
+    printf("%d",i);
+    }
