c55b327533cf4f7a64aa775d2c00f0c712373913
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-03-23 12:01:28 +0530

    Create set3_3.c

---
+++ set3_3.c
@@ -0,0 +1,25 @@
+int main()
+{
+    int i,a[20],n,t=0,j;
+    printf("enter the n:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+    if(a[i]>a[j])
+    {
+        t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+    }
+    }
+}
+    printf("%d",a[0]);
+
+    return 0;
+}
