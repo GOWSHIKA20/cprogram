5ba08dda6e3ea914ce735193eec7af7ba15b83b3
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-03-27 21:34:14 +0530

    Create set4_4.c

---
+++ set4_4.c
@@ -0,0 +1,14 @@
+int main()
+{
+	int a=0,c,i;
+	char b[100];
+	printf("enter the string:");
+	scanf("%[^\n]s",&b);
+	for(i=0;b[i]!='\0';i++)
+	{
+		if(b[i]=='.')
+		a++;
+		}
+		printf("%d",a+1);
+return 0;
+}
