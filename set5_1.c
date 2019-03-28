386b7ea8da80ec8253f0faad7088c2a5716f7d2c
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-03-28 20:33:43 +0530

    Create set5_1.c

---
+++ set5_1.c
@@ -0,0 +1,15 @@
+int main()
+{
+    char s[20];
+    int i,k;
+    printf("enter the string:");
+    scanf("%s",s);
+    printf("enter the k:");
+    scanf("%d",&k);
+    for(i=0;i<=k;i++)
+    {
+        printf("%s\n",s);
+    }
+    
+return 0;
+}
