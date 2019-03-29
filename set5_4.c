8e30ac5815f7b95531299498fcba132e6ca360d1
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-03-29 07:11:24 +0530

    Create set5_4.c

---
+++ set5_4.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int n;
+    printf("enter the n:");
+    scanf("%d",&n);
+    if(n>=1&&n<=10)
+    {
+        printf("yes");
+    }
+        else
+        {
+        printf("no");
+        }
+
+    return 0;
+}
