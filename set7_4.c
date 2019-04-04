9a8c5c05e6f147a27436abf2f0327fac24c2a9f4
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-04-04 22:44:06 +0530

    Create set7_4.c

---
+++ set7_4.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int n,m,s=0;
+    printf("enter the n&m:");
+    scanf("%d%d",&n,&m);
+    s=n+m;
+    if(s%2==0)
+    printf("even");
+    else
+    printf("odd");
+    return 0;
+}
