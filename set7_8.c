4eecb103390c0592dde291053ec385abe883558c
GOWSHIKA20 <47447284+GOWSHIKA20@users.noreply.github.com>
2019-04-06 20:51:51 +0530

    Create set7_8.c

---
+++ set7_8.c
@@ -0,0 +1,23 @@
+int main()
+{
+
+    
+int n;
+printf("enter the n:");
+    scanf("%d",&n);
+    if(n!=0)
+    {
+    if(n%7==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    }
+    
+    
+    
+ return 0;
+}
